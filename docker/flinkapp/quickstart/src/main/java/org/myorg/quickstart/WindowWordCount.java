3cd65500ca4f4e07fa93207effc2171240dbcb2d
VincentWijshoff <vincent.wijshoff@gmail.com>
2022-08-10 12:25:44 +0200

    remove merge conflict

--- docker/flinkapp/quickstart/src/main/java/org/myorg/quickstart/WindowWordCount.java
+++ docker/flinkapp/quickstart/src/main/java/org/myorg/quickstart/WindowWordCount.java
@@ -25,7 +25,7 @@ public class WindowWordCount {
 
 
         KafkaSource<String> source = KafkaSource.<String>builder()
-                .setBootstrapServers("10.43.244.207:9092")
+                .setBootstrapServers("yalii-cluster-kafka-bootstrap:9092")
                 .setTopics("vincent-input")
                 .setGroupId("my-group")
                 .setStartingOffsets(OffsetsInitializer.earliest())
@@ -53,9 +53,6 @@ public class WindowWordCount {
                 .build();
 
         dataStream.sinkTo(sink);
-
-        // start a checkpoint every 1000 ms
-        // env.enableCheckpointing(1000);
 
         env.execute("Window WordCount");
     }
